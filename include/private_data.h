ac883c316a6c74103f7ea2a1d6aca33a280b02ee
MostafaQusit <1803215@eng.asu.edu.eg>
2023-06-26 14:28:43 +0300

    semi-final

---
+++ include/private_data.h
@@ -0,0 +1,12 @@
+#ifndef PRIVATE_DATA_H_
+#define PRIVATE_DATA_H_
+
+// all private data that no need to put them in main code
+
+#define HOME1_SSID "Koset"
+#define HOME1_PASS "h9f16306"
+
+#define HOME2_SSID "WE_F6AE4C"
+#define HOME2_PASS "lcw04660"
+
+#endif
